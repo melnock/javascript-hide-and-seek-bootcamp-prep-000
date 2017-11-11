6ee437393bbc9429b1bf702d9603fdea7a272127
Melissa Nock <melnock@gmail.com>
2017-11-11 21:22:33 +0000

    Done.

--- index.js
+++ index.js
@@ -1,7 +1,3 @@
 function getFirstSelector(selector) {
   return document.getElementById("app").querySelector(selector);
 }
-
-function nestedTarget(){
-  return document.getElementById("nested").querySelectorAll(“div.target”);
-}
